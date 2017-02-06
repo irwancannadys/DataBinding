fecb9b02a089c22ac377a0905e01af50b29c6776
qurrata <appsnjs@gmail.com>
2017-02-06 10:32:03 +0700

    Initial commit Data Binding Tester

---
+++ app/src/main/java/com/example/irwancannady/databinding/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.irwancannady.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.irwancannady.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        User user = new User();
+        user.setUsername("Irwan");
+        binding.setUser(user);
+        binding.setActivity(this);
+    }
+
+    public void onButtonClick(String email){
+
+        Log.d(TAG, "Email : " + binding.getUser().getEmail());
+        Log.d(TAG, "Email : " + email);
+        Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/irwancannady/databinding/User.java
@@ -0,0 +1,29 @@
+package com.example.irwancannady.databinding;
+// Created by irwancannady (irwancannady@gmail.com) on 2/6/17 at 10:19 AM.
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class User extends BaseObservable {
+
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(com.example.irwancannady.databinding.BR.email);
+    }
+}
