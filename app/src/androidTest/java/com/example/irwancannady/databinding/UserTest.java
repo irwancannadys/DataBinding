159766c782ecd6cc972abe0128d2d6edfda9508f
qurrata <appsnjs@gmail.com>
2017-02-06 10:48:36 +0700

    testingunit

---
+++ app/src/androidTest/java/com/example/irwancannady/databinding/UserTest.java
@@ -0,0 +1,27 @@
+package com.example.irwancannady.databinding;
+
+import org.junit.Test;
+
+// Created by irwancannady (irwancannady@gmail.com) on 2/6/17 at 10:43 AM.
+public class UserTest {
+    @Test
+    public void getUsername() throws Exception {
+
+    }
+
+    @Test
+    public void setUsername() throws Exception {
+
+    }
+
+    @Test
+    public void getEmail() throws Exception {
+
+    }
+
+    @Test
+    public void setEmail() throws Exception {
+
+    }
+
+}
